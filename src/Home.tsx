1ec177af9578929b3fdf8e646a620329b58ea09d
Yaytay <jim-gitkraken@spudsoft.co.uk>
2023-12-22 07:55:42 +0000

    Layout home buttons nicely

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,10 +13,10 @@ interface HomeProps {
 function Home(props : HomeProps) {
     return (
         <div className="grid h-screen place-content-center">
-            <div>
+            <div className="flex flex-wrap place-content-center">
                 { props.designMode && 
                     <Link to="/ui/design">
-                        <div className="w-80 bg-zinc-100 p-5 m-10 float-left text-center">
+                        <div className="w-80 h-64 bg-zinc-100 p-5 m-10 float-left text-center">
                             <h2>Design</h2>
                             The design view lets you edit pipelines, and also change any other
                             files accessible to the query engine.
@@ -25,7 +25,7 @@ function Home(props : HomeProps) {
                 }
                 { props.available && 
                     <Link to="/ui/test">
-                        <div className="w-80 bg-zinc-100 p-5 m-10 float-left text-center">
+                        <div className="w-80 h-64 bg-zinc-100 p-5 m-10 float-left text-center">
                             <h2>Test</h2>
                             The test view lets you run a pipeline repeatedly and see the results
                             immediately.
@@ -34,7 +34,7 @@ function Home(props : HomeProps) {
                 }
                 { props.managementEndpoints && 
                     <Link to="/ui/manage">
-                        <div className="w-80 bg-zinc-100 p-5 m-10 float-left text-center">
+                        <div className="w-80 h-64 bg-zinc-100 p-5 m-10 float-left text-center">
                             <h2>Manage</h2>
                             Management endpoints for controlling or debugging the service.
                         </div>
@@ -42,7 +42,7 @@ function Home(props : HomeProps) {
                 }
                 { props.docs && 
                     <Link to="/ui/help">
-                        <div className="w-80 bg-zinc-100 p-5 m-10 float-left text-center">
+                        <div className="w-80 h-64 bg-zinc-100 p-5 m-10 float-left text-center">
                             <h2>Documentation</h2>
                             Access to the Query Engine documentation.
                         </div>
@@ -50,7 +50,7 @@ function Home(props : HomeProps) {
                 }
                 { props.apiUrl && 
                     <Link to="/ui/api">
-                        <div className="w-80 bg-zinc-100 p-5 m-10 float-left text-center">
+                        <div className="w-80 h-64 bg-zinc-100 p-5 m-10 float-left text-center">
                             <h2>API</h2>
                             API Documentation.
                         </div>
