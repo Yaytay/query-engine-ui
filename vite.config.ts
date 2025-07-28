2e0d6b8de065d2c3d914f63976974d92a3fc33ce
Yaytay <jim-github@spudsoft.co.uk>
2025-07-28 11:29:28 +0100

    Attempt to fix QE icon and CSP

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,11 @@ export default defineConfig({
     open: true,
     port: 5173,
     headers: {
-      'Content-Security-Policy': "default-src 'self' http://localhost:8000/; img-src 'self'; style-src 'self' 'unsafe-inline' 'unsafe-hashes' https://cdn.form.io/; connect-src 'self' http://localhost:8000/ http://localhost:8001/; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.form.io/"
+      'Content-Security-Policy': "default-src 'self' http://localhost:8000/;"
+           + " img-src 'self';"
+           + " style-src 'self' 'unsafe-inline' 'unsafe-hashes' https://cdn.form.io/;"
+           + " connect-src 'self' http://localhost:8000/ http://localhost:8001/;"
+           + " script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.form.io/"
       , 'Permissions-Policy': "accelerometer=(), autoplay=(), camera=(), clipboard-read=(), clipboard-write=(), display-capture=(), document-domain=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), microphone=(), magnetometer=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), serial=(), usb=(), web-share=(), xr-spatial-tracking=()"
       , 'Referrer-Policy': "same-origin"
       , 'Strict-Transport-Security': "max-age=63072000"
@@ -29,5 +33,11 @@ export default defineConfig({
   },
   build: {
     assetsInlineLimit: 0 // 👈 disables inlining of small assets
+    , rollupOptions: {
+      output: {
+        assetFileNames: '[name].[hash][extname]',
+      },
+    },
   }
+
 })
